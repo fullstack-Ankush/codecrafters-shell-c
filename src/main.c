a851dc073301bb83f853cc889d7520f93c652c1a
Ankush Pal <24cs118@lingayasvidyapeeth.edu.in>
2026-03-01 11:12:53 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,62 +3,48 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
-  char command[100];
-  while(1){
-    printf("$ ");
-    fgets(command ,100,stdin);
-    command[strlen(command)-1] = '\0';
-    if(strcmp(command,"exit") == 0)
-      exit(0);
-    if(strncmp(command,"echo ",5) == 0){
-      printf("%s\n",command+5);
-      continue;
-    }
-    if(strncmp(command,"type ",5) == 0){
-      char *cmd = command + 5;
-      if(strcmp(cmd,"echo")==0 || strcmp(cmd,"type")==0 || strcmp(cmd,"exit")== 0) {
-        printf("%s is a shell builtin\n",cmd);
-        continue;
-      }
-      else{
-        // Search for command in PATH
-        char *path = getenv("PATH");
-        if(path == NULL) {
-          printf("%s: not found\n",cmd);
-          continue;
-        }
-        
-        char path_copy[1000];
-        strcpy(path_copy, path);
-        
-        char *dir = strtok(path_copy, ":");
-        int found = 0;
-        
-        while(dir != NULL) {
-          char full_path[256];
-          snprintf(full_path, sizeof(full_path), "%s/%s", dir, cmd);
-          
-          // Check if file exists and is executable
-          if(access(full_path, X_OK) == 0) {
-            printf("%s is %s\n", cmd, full_path);
-            found = 1;
-            break;
-          }
-          dir = strtok(NULL, ":");
-        }
-        
-        if(!found) {
-          printf("%s: not found\n",cmd);
-        }
-        continue;
-      }
-    } 
-    printf("%s: command not found \n",command);
-  }
-  
+#ifdef _WIN32
+#define PATH_SEP ";"
+#else
+#define PATH_SEP ":"
+#endif
 
-  return 0;
-}
+int find_file(char *arg, char *path_env, int verbose);
+
+int main(int argc, char *argv[]) {
+  char cmd[100];
+  char *path_env;
+  char input[999];
+  char tmp_input[999];
+  char *tok;
+
+  path_env = getenv("PATH");
+  setbuf(stdout, NULL);
+  while (1) {
+    printf("$ ");
+    fgets(input, 999, stdin);
+    input[strlen(input) - 1] = '\0';
+    strcpy(tmp_input, input);
+    tok = strtok(tmp_input, " ");
+    if (strcmp(tok, "exit") == 0)
+      return (0);
+    else if (strcmp(tok, "type") == 0) {
+      tok = strtok(NULL, " ");
+      if (strcmp(tok, "echo") == 0 || strcmp(tok, "exit") == 0 ||
+          strcmp(tok, "type") == 0) {
+        printf("%s is a shell builtin\n", tok);
+        continue;
+      }
+      find_file(tok, path_env, 1);
+    } else if (strcmp(tok, "echo") == 0) {
+      tok = strtok(NULL, " ");
+      while (tok != NULL) {
+        printf("%s ", tok);
+        tok = strtok(NULL, " ");
+      }
+      printf("\n");
+    } else if (find_file(tok, path_env, 0))
+      system(input);
+    else
+      printf("%s: command not found\n", tmp_input);
+  }
