22607b1bb69b46ed73625831bc3f0ac7c6cb287b
Ankush Pal <24cs118@lingayasvidyapeeth.edu.in>
2026-03-01 11:05:56 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -17,24 +18,47 @@ int main(int argc, char *argv[]) {
       continue;
     }
     if(strncmp(command,"type ",5) == 0){
-      char *cmd = command +5;
+      char *cmd = command + 5;
       if(strcmp(cmd,"echo")==0 || strcmp(cmd,"type")==0 || strcmp(cmd,"exit")== 0) {
         printf("%s is a shell builtin\n",cmd);
         continue;
-
       }
       else{
-        printf("%s: not found\n",cmd);
+        // Search for command in PATH
+        char *path = getenv("PATH");
+        if(path == NULL) {
+          printf("%s: not found\n",cmd);
+          continue;
+        }
+        
+        char path_copy[1000];
+        strcpy(path_copy, path);
+        
+        char *dir = strtok(path_copy, ":");
+        int found = 0;
+        
+        while(dir != NULL) {
+          char full_path[256];
+          snprintf(full_path, sizeof(full_path), "%s/%s", dir, cmd);
+          
+          // Check if file exists and is executable
+          if(access(full_path, X_OK) == 0) {
+            printf("%s is %s\n", cmd, full_path);
+            found = 1;
+            break;
+          }
+          dir = strtok(NULL, ":");
+        }
+        
+        if(!found) {
+          printf("%s: not found\n",cmd);
+        }
         continue;
       }
     } 
     printf("%s: command not found \n",command);
   }
   
-
-  // TODO: Uncomment the code below to pass the first stage
-
-
 
   return 0;
 }
